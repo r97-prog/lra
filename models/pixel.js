10d16ede0b664bb01ae4f6dde6a58a81e1a9d761
ravi.mali <ravi.mali@pixelnx.com>
2021-08-19 18:59:29 +0530

    first commit

---
+++ models/pixel.js
@@ -0,0 +1,20 @@
+var mongoose = require("mongoose");
+
+const pixelschema = new mongoose.Schema({
+    fullName:{
+        type:String,
+    },
+    email:{
+        type:String,
+    },
+    password:{
+        type:String,
+    },
+    confirmPass:{
+        type:String,
+    }
+});
+
+const pixel  = new mongoose.model('pixel',pixelschema);
+
+module.exports = pixel;
